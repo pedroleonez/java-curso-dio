ae782e722af000bd573c4be90a716e88ba295e65
pedroleonez <pedroleonez1@gmail.com>
2024-08-09 00:45:44 -0300

    MC

--- SMART-TV/src/TiposVariaveis.java
+++ SMART-TV/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        String nome = "pedro" + "leonez";
+
+        System.out.println(nome);
     }
 }
