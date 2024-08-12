a5b3e3f5565042689a2e3e7af89ea1b5f9b93e4e
pedroleonez <pedroleonez1@gmail.com>
2024-08-12 03:34:37 -0300

    começando ex003

---
+++ EXERCICIOS-CONDICIONAIS/src/For_Ex003.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class For_Ex003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        
+
+        sc.close();
+        
+    }
+}
