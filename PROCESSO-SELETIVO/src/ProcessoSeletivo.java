f23a311248d2b69c66f8b1ba2cd910838d04f29a
pedroleonez <pedroleonez1@gmail.com>
2024-08-15 21:24:39 -0300

    Update ProcessoSeletivo.java

--- PROCESSO-SELETIVO/src/ProcessoSeletivo.java
+++ PROCESSO-SELETIVO/src/ProcessoSeletivo.java
@@ -1,8 +1,39 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        imprimirSelecioandos();
+        String [] candidatos = {"ANA", "BRENA", "CARLA", "DAIANE", "EMANUELE"};
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato (String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            // elas precisarão sofrer alterações
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+            }
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.println("CONSEGUIMOS CONTANTO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA.");
+        } else {
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS REALIZADO: " + tentativasRealizadas);
+        }
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecioandos() {
